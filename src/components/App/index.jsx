9517288cb2d4d6cad0c03f06682056e2f6874653
nimaaskarian <nimaas1400@gmail.com>
2022-04-04 03:43:07 +0430

    added chartjs

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,14 +1,54 @@
 import FloatingCard from "blocks/FloatingCard";
 import React from "react";
 import Theme from "Theme";
-
 import { StyledApp } from "./style";
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  LineController,
+} from "chart.js";
+import { Chart } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  LineController,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+ChartJS.register(LineController, LineElement, PointElement, LinearScale, Title);
+
 const App = () => {
+  const labels = ["January", "February", "March", "April", "May", "June"];
+  const chartData = {
+    labels: labels,
+    datasets: [
+      {
+        label: "My First dataset",
+        backgroundColor: "rgb(255, 99, 132)",
+        borderColor: "rgb(255, 99, 132)",
+        data: [0, 10, 5, 2, 20, 30, 45],
+      },
+    ],
+  };
   return (
     <Theme>
       <StyledApp>
         <FloatingCard area="navbar"></FloatingCard>
-        <FloatingCard area="first"></FloatingCard>
+        <FloatingCard area="first">
+          <Chart type="line" data={chartData} />
+        </FloatingCard>
         <FloatingCard area="second"></FloatingCard>
         <FloatingCard area="third"></FloatingCard>
         <FloatingCard area="forth"></FloatingCard>
--- src/components/App/style.js
+++ src/components/App/style.js
@@ -9,6 +9,8 @@ export const StyledApp = styled.div`
   padding: 20px;
   display: grid;
   grid-gap: 15px;
-  grid-template-rows: 50px 1fr 1fr 1fr;
+  grid-template-rows: 50px 50% 1fr 1fr;
+  grid-template-columns: 1fr 40% 1fr 1fr;
+
   grid-template-areas: "navbar navbar navbar navbar" "first second second third" "forth forth fifth sixth" "forth forth seventh eighth";
 `;
